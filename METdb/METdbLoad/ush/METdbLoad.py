2a55a82b049b6f1312720099993ba077e357eb5c
Venita Hagerty <venita.hagerty@noaa.gov>
2019-04-19 15:23:57 -0600

    Did some cleanup of code based on pylint comments

--- METdb/METdbLoad/ush/METdbLoad.py
+++ METdb/METdbLoad/ush/METdbLoad.py
@@ -9,25 +9,30 @@ Usage: Load files into METdb
 Parameters: -index
 Input Files: load_spec XML file
 Output Files: N/A
-Copyright 2019 UCAR/NCAR/RAL, Colorado State University, Regents of the University of Colorado, NOAA/OAR/ESRL/GSD
+Copyright 2019 UCAR/NCAR/RAL, CSU/CIRES, Regents of the University of Colorado, NOAA/OAR/ESRL/GSD
 '''
 
 import argparse
 import sys
 import pymysql
 
-import METdb.METdbLoad.ush.readLoadXML as RX
+import METdb.METdbLoad.ush.read_load_xml as RX
+
 
 def main():
+    """! Class to read in load_spec xml file
+        Returns:
+           N/A
+    """
 
     print("--- Start METdbLoad ---")
 
     parser = argparse.ArgumentParser()
     parser.add_argument("xmlfile", help="please provide required xml load_spec filename")
-    parser.add_argument("-index", action="store_true", help="only process index commands, do not load data")
+    parser.add_argument("-index", action="store_true", help="only process index, do not load data")
 
     # get the command line arguments
-    args =  parser.parse_args()
+    args = parser.parse_args()
 
     print(sys.path)
 
@@ -36,18 +41,15 @@ def main():
         print("index is true - only process index")
 
     try:
-        print ("XML filename is ", args.xmlfile)
+        print("XML filename is ", args.xmlfile)
 
         # instantiate a load_spec XML file
         xml_loadfile = RX.XmlLoadFile(args.xmlfile)
 
         # read in the XML file and get the information out of its tags
         xml_loadfile.read_xml()
-    except:
+    except (RuntimeError, TypeError, NameError):
         print("***", sys.exc_info()[0], "occurred in Main ***")
-
-    if not xml_loadfile.stat_header_db_check:
-        print("Do not check stat headers")
 
     print("--- End METdbLoad ---")
 
--- METdb/METdbLoad/ush/read_load_xml.py
+++ METdb/METdbLoad/ush/read_load_xml.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 '''
-Program Name: readLoadXML.py
+Program Name: read_load_xml.py
 Contact(s): Venita Hagerty
 Abstract:
 History Log:  Initial version
@@ -9,7 +9,7 @@ Usage: Read load_spec XML file
 Parameters: N/A
 Input Files: load_spec XML file
 Output Files: N/A
-Copyright 2019 UCAR/NCAR/RAL, Colorado State University, Regents of the University of Colorado, NOAA/OAR/ESRL/GSD
+Copyright 2019 UCAR/NCAR/RAL, CSU/CIRES, Regents of the University of Colorado, NOAA/OAR/ESRL/GSD
 '''
 
 import sys
@@ -19,6 +19,11 @@ import METdb.METdbLoad.ush.constants as CN
 
 
 class XmlLoadFile:
+    """! Class to read in load_spec xml file
+        Returns:
+           N/A
+    """
+
     def __init__(self, xmlfile):
         # set the defaults
         self.xmlfilename = xmlfile
@@ -48,25 +53,29 @@ class XmlLoadFile:
         self.group = CN.DEFAULT_DATABASE_GROUP
         self.description = None
 
+        self.load_files = []
+        self.line_types = []
+
     def read_xml(self):
+        """! Read in load_spec xml file, sotre values as attributes
+            Returns:
+               N/A
+        """
         try:
             # parse the XML file
             tree = etree.parse(self.xmlfilename)
             root = tree.getroot()
-            print (root.tag)
-        except:
-            print("Couldn't read XML file")
-
-        self.load_files = []
-        self.line_types = []
+            print(root.tag)
+        except (RuntimeError, TypeError, NameError):
+            print("***", sys.exc_info()[0], "in", "read_xml", "***")
 
         folder_template = None
         template_fillins = {}
 
         try:
-            # extract the values from the load_spec XML tags and store them in attributes of class XmlLoadFile
+            # extract values from load_spec XML tags, store in attributes of class XmlLoadFile
             for child in root:
-                print ("child: ", child.tag, child.text)
+                print("child: ", child.tag, child.text)
                 for subchild in list(child):
                     print("subchild: ", subchild.tag, subchild.text)
                 if child.tag.lower() == "connection":
@@ -95,40 +104,40 @@ class XmlLoadFile:
                             template_values.append(template_value.text)
                         template_fillins[template_key] = template_values
                 elif child.tag.lower() == "verbose":
-                    if child.text.lower() == "true":
+                    if child.text.lower() == CN.LCTRUE:
                         self.verbose = True
                 elif child.tag.lower() == "drop_indexes":
-                    if child.text.lower() == "true":
+                    if child.text.lower() == CN.LCTRUE:
                         self.drop_indexes = True
                 elif child.tag.lower() == "apply_indexes":
-                    if child.text.lower() == "false":
+                    if child.text.lower() == CN.LCFALSE:
                         self.apply_indexes = False
                 elif child.tag.lower() == "stat_header_db_check":
-                    if child.text.lower() == "false":
+                    if child.text.lower() == CN.LCFALSE:
                         self.stat_header_db_check = False
                 elif child.tag.lower() == "mode_header_db_check":
-                    if child.text.lower() == "false":
+                    if child.text.lower() == CN.LCFALSE:
                         self.mode_header_db_check = False
                 elif child.tag.lower() == "mtd_header_db_check":
-                    if child.text.lower() == "false":
+                    if child.text.lower() == CN.LCFALSE:
                         self.mtd_header_db_check = False
                 elif child.tag.lower() == "load_stat":
-                    if child.text.lower() == "false":
+                    if child.text.lower() == CN.LCFALSE:
                         self.load_stat = False
                 elif child.tag.lower() == "load_mode":
-                    if child.text.lower() == "false":
+                    if child.text.lower() == CN.LCFALSE:
                         self.load_mode = False
                 elif child.tag.lower() == "load_mtd":
-                    if child.text.lower() == "false":
+                    if child.text.lower() == CN.LCFALSE:
                         self.load_mtd = False
                 elif child.tag.lower() == "load_mpr":
-                    if child.text.lower() == "true":
+                    if child.text.lower() == CN.LCTRUE:
                         self.load_mpr = True
                 elif child.tag.lower() == "load_orank":
-                    if child.text.lower() == "true":
+                    if child.text.lower() == CN.LCTRUE:
                         self.load_orank = True
                 elif child.tag.lower() == "force_dup_file":
-                    if child.text.lower() == "true":
+                    if child.text.lower() == CN.LCTRUE:
                         self.force_dup_file = True
                 elif child.tag.lower() == "insert_size":
                     if child.text.isdigit():
@@ -142,7 +151,7 @@ class XmlLoadFile:
                 elif child.tag.lower() == "load_note":
                     self.load_note = child.text
                 elif child.tag.lower() == "load_xml":
-                    if child.text.lower() == "false":
+                    if child.text.lower() == CN.LCFALSE:
                         self.load_xml = False
                 # MET line types to load. If omitted, all line types are loaded
                 elif child.tag.lower() == "line_type":
@@ -151,64 +160,58 @@ class XmlLoadFile:
                         self.line_types.append(subchild.text)
                 else:
                     print("unknown tag:", child.tag)
-        except:
-            print("***", sys.exc_info()[0], "occurred while reading xml values in readLoadXML ***")
+        except (RuntimeError, TypeError, NameError):
+            print("***", sys.exc_info()[0], "in", "read_xml", "***")
 
 
         print("group is:", self.group)
         print("db_name is:", self.db_name)
 
         if folder_template is not None:
-            self.load_files = self.generate_filenames_from_template(folder_template, template_fillins)
+            self.load_files = self.filenames_from_template(folder_template, template_fillins)
 
         print("load_files are:", len(self.load_files), self.load_files)
 
-    def generate_filenames_from_template(self, folder_template, template_fillins):
+    def filenames_from_template(self, folder_template, template_fillins):
         """! given a folder template and the values to fill in, generates list of filenames
             Returns:
                list of filenames
         """
         print("folder template is:", folder_template)
-        print("template_fillins are:", template_fillins)
 
         try:
 
             fillins_open = folder_template.count("{")
-            fillins_close = folder_template.count("}")
-            if fillins_open == fillins_close:
-                # remove any values that are not in the template
-                if len(template_fillins) > 0:
-                    copy_template_fillins = dict(template_fillins)
-                    for key in copy_template_fillins:
-                        if key not in folder_template:
-                            del template_fillins[key]
-                if fillins_open > len(template_fillins):
-                    raise ValueError("not enough template fillin values")
-                # generate a list of directories with all combinations of values filled in
-                load_dirs = [folder_template]
-                for key in template_fillins:
-                    alist = []
-                    for fvalue in template_fillins[key]:
-                        for wvalue in load_dirs:
-                            wstring = wvalue.replace("{" + key + "}", fvalue)
-                            alist.append(wstring)
-                    load_dirs = alist
-                # find all the files in the directories, append path to them, and put on load_files list
-                file_list = []
-                for file_dir in load_dirs:
-                    for file_name in os.listdir(file_dir):
-                        file_list.append(file_dir + "/" + file_name)
-                print("used template_fillins are:", template_fillins)
-                print("load_dirs", load_dirs)
-                return file_list
-            else:
+            if fillins_open != folder_template.count("}"):
                 raise ValueError("mismatched curly braces")
-        except ValueError as ve:
-            print("***", sys.exc_info()[0], "occurred in readLoadXML ***")
-            print(ve)
-        except:
-            print("***", sys.exc_info()[0], "occurred while processing folder template in readLoadXML ***")
+            # remove any values that are not in the template
+            if template_fillins:
+                copy_template_fillins = dict(template_fillins)
+                for key in copy_template_fillins:
+                    if key not in folder_template:
+                        del template_fillins[key]
+            if fillins_open > len(template_fillins):
+                raise ValueError("not enough template fillin values")
+            # generate a list of directories with all combinations of values filled in
+            load_dirs = [folder_template]
+            for key in template_fillins:
+                alist = []
+                for fvalue in template_fillins[key]:
+                    for wvalue in load_dirs:
+                        alist.append(wvalue.replace("{" + key + "}", fvalue))
+                load_dirs = alist
+            # find all files in directories, append path to them, and put on load_files list
+            file_list = []
+            for file_dir in load_dirs:
+                for file_name in os.listdir(file_dir):
+                    file_list.append(file_dir + "/" + file_name)
+            print("template_fillins are:", template_fillins)
+            print("load_dirs", load_dirs)
 
+        except ValueError as value_error:
+            print("***", sys.exc_info()[0], "in", "filenames_from_template", "***")
+            print(value_error)
+        except (RuntimeError, TypeError, NameError):
+            print("***", sys.exc_info()[0], "in", "filenames_from_template", "***")
 
-
-
+        return file_list
