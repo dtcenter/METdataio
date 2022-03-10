3dd96b3e1fda30946312f89d29594f8cec8c6128
Hank Fisher <fisherh@ucar.edu>
2022-03-10 06:39:46 -0700

    Updated version and release notes for rc1

--- METdbLoad/setup.py
+++ METdbLoad/setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='METdbLoad',
-    version='0.1.0',
+    version='1.1.0',
     description='Rewrite of Java MVLoad for METviewer and METexpress database load',
     author='Venita Hagerty',
     author_email='venita.hagerty@noaa.gov',
