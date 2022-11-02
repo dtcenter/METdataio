d4233492cb1bb83f07d0be5ce9038099c0b56493
bikegeek <minnawin@ucar.edu>
2022-11-02 11:59:53 -0600

    Added type annotations

--- METreformat/write_stat_ascii.py
+++ METreformat/write_stat_ascii.py
@@ -20,6 +20,7 @@ import os
 import logging
 import time
 from datetime import timedelta
+from typing import List, Set
 import numpy as np
 import pandas as pd
 import yaml
@@ -29,6 +30,7 @@ from METdbLoad.ush.read_load_xml import XmlLoadFile
 from METdbLoad.ush.read_data_files import ReadDataFiles
 import util
 
+
 class WriteStatAscii:
     """ Class to write MET .stat files to an ASCII file
 
@@ -36,17 +38,15 @@ class WriteStatAscii:
            None. Generates an output file
     """
 
-    def write_stat_ascii(self, load_flags, stat_data, parms):
+    def write_stat_ascii(self, stat_data:pd.DataFrame, parms:dict):
         """ write MET stat files (.stat) to an ASCII file with stat_name, stat_value, stat_bcl, stat_bcu,
             stat_ncl, and stat_ncu columns, converting the original data file from wide form to long form.
 
 
             Args:
-                @param load_flags: flag values set in the XML spec file, indicating which items
-                            are to be loaded
                 @ param stat_data: pandas dataframe corresponding to the MET stat input file
 
-                @param parms:  The yaml configuration object containing the settings for output dir, output file
+                @param parms:  The yaml configuration object (dictionary) containing the settings for output dir, output file
 
             Returns:  None, write an output ASCII file associated with the original MET .stat file with statistics
                       information aggregated into these six columns: stat_name, stat_value, stat_ncl, stat_ncu,
@@ -57,10 +57,9 @@ class WriteStatAscii:
 
         logging.debug("[--- Start write_stat_data ---]")
 
-        write_time_start = time.perf_counter()
+        write_time_start:float = time.perf_counter()
 
         try:
-
 
             # --------------------
             # Write Stat Headers
@@ -70,22 +69,23 @@ class WriteStatAscii:
             # (columns 1-14, then create headers for the maximum number of allowable
             # MET stat headers). The FCST_INIT_BEG header is added in after the FCST_VALID_END
             # column, so there is one additional column to the common header.
-            common_stat_headers = cn.LC_COMMON_STAT_HEADER
-            line_types = list(stat_data['line_type'])
-            unique_line_types = set(line_types)
+            common_stat_headers:str = cn.LC_COMMON_STAT_HEADER
+            line_types:List[str] = list(stat_data['line_type'])
+            unique_line_types:Set[str] = set(line_types)
 
             # ------------------------------
             # Extract statistics information
             # ------------------------------
             # For each line type, extract the statistics information and save it in a new dataframe
-            expanded_headers = common_stat_headers + ['stat_name', 'stat_value', 'stat_bcl', 'stat_bcu', 'stat_ncl,'
-                                                      'stat_ncu']
-            all_var = pd.DataFrame(columns=expanded_headers)
+            expanded_headers:List[str] = common_stat_headers + ['stat_name', 'stat_value', 'stat_bcl', 'stat_bcu', 'stat_ncl,'
+                                                                                                         'stat_ncu']
+            all_var:pd.DataFrame = pd.DataFrame(columns=expanded_headers)
 
             for idx, cur_line_type in enumerate(unique_line_types):
                 if cur_line_type == cn.FHO:
-                    fho_var = self.process_by_stat_linetype(cur_line_type, stat_data)
+                    fho_var:pd.DataFrame = self.process_by_stat_linetype(cur_line_type, stat_data)
                 if cur_line_type == cn.CNT:
+                    pass
                     cnt_var = self.process_by_stat_linetype(cur_line_type, stat_data)
 
             # ToDo
@@ -93,11 +93,11 @@ class WriteStatAscii:
             #
 
             # Write out to an ASCII file
-            out_path = parms['output_dir']
-            out_filename = parms['output_filename']
-            output_file = os.path.join(out_path, out_filename)
+            out_path:str = parms['output_dir']
+            out_filename:str = parms['output_filename']
+            output_file:os.path = os.path.join(out_path, out_filename)
             print(f"Writing output...")
-            final_df = fho_var.to_csv(output_file, index=None, sep='\t', mode='a')
+            final_df:pd.DataFrame = fho_var.to_csv(output_file, index=None, sep='\t', mode='a')
 
 
 
@@ -105,14 +105,14 @@ class WriteStatAscii:
         except (RuntimeError, TypeError, NameError, KeyError):
             logging.error("*** %s in write_stat_ascii ***", sys.exc_info()[0])
 
-        write_time_end = time.perf_counter()
-        write_time = timedelta(seconds=write_time_end - write_time_start)
+        write_time_end:float = time.perf_counter()
+        write_time:timedelta = timedelta(seconds=write_time_end - write_time_start)
 
         logging.info("    >>> Write time Stat: %s", str(write_time))
 
         logging.debug("[--- End write_stat_data ---]")
 
-    def process_by_stat_linetype(self, linetype, stat_data):
+    def process_by_stat_linetype(self, linetype:str, stat_data:pd.DataFrame):
         '''
            For a given linetype, extract the relevant statistics information into the
            stat_name, stat_value, stat_bcl, stat_bcu, stat_ncl, and stat_ncu columns,
@@ -128,14 +128,13 @@ class WriteStatAscii:
         '''
 
         # FHO forecast, hit rate, observation rate
-        linetype_data = self.process_fho(stat_data)
-
+        linetype_data:pd.DataFrame = self.process_fho(stat_data)
 
         # CNT Continuous Statistics
         if linetype == cn.CNT:
             # Relevant columns for the CNT line type
-            cnt_columns_to_use = np.arange(0, 126).tolist()
-            cnt_df = stat_data[stat_data['line_type'] == linetype].iloc[:, cnt_columns_to_use]
+            cnt_columns_to_use:List[str] = np.arange(0, 126).tolist()
+            cnt_df:pd.DataFrame = stat_data[stat_data['line_type'] == linetype].iloc[:, cnt_columns_to_use]
 
             pass
 
@@ -153,16 +152,20 @@ class WriteStatAscii:
 
         return linetype_data
 
-    def process_fho(self, stat_data ):
+    def process_fho(self, stat_data:pd.DataFrame) -> pd.DataFrame:
+        '''
+            Retrieve the FHO line type data and reshape it to replace the original columns (based on column number) into
+            stat_name, stat_value, stat_bcl, stat_bcu, stat_ncu, and stat_ncl
+
+            Arguments:
+            @param stat_data: The dataframe containing all the original data from the MET .stat file.
+
+            Returns:
+            linetype_data:  The dataframe with the reshaped data for the FHO line type
         '''
 
-        :param stat_data:
-        :return:
-        '''
+        linetype:str = cn.FHO
 
-        linetype = cn.FHO
-
-        # if linetype == cn.FHO:
         # Extract the stat_names and stat_values for this line type:
         # F_RATE, H_RATE, O_RATE (these will be the stat name).  There are no corresponding xyz_bcl, xyz_bcu,
         # xyz_ncl, and xyz_ncu values where xyz = stat name
@@ -173,15 +176,16 @@ class WriteStatAscii:
         #
 
         # Relevant columns for the FHO line type
-        fho_columns_to_use = np.arange(0, 29).tolist()
-        fho_df = stat_data[stat_data['line_type'] == linetype].iloc[:, fho_columns_to_use]
+        fho_columns_to_use:List[str] = np.arange(0, 29).tolist()
+        fho_df:pd.DataFrame = stat_data[stat_data['line_type'] == linetype].iloc[:, fho_columns_to_use]
 
         # Add the stat columns for the FHO line type
-        fho_columns = cn.LC_COMMON_STAT_HEADER + ['total', 'F_RATE', 'H_RATE', 'O_RATE']
-        fho_df.columns = fho_columns
+        fho_columns:List[str] = cn.LC_COMMON_STAT_HEADER + ['total', 'F_RATE', 'H_RATE', 'O_RATE']
+        fho_df.columns:List[str] = fho_columns
 
-        # Create another index column to preserve the original index values from the stat_data dataframe
-        idx = list(fho_df.index)
+        # Create another index column to preserve the index values from the stat_data dataframe (ie the dataframe
+        # containing the original data from the MET output file).
+        idx:int = list(fho_df.index)
         fho_df.insert(loc=0, column='Idx', value=idx)
 
         # Use pandas 'melt' to reshape the data frame from wide to long shape (i.e. collecting the f_rate, h_rate,
@@ -189,20 +193,27 @@ class WriteStatAscii:
         # containing the names F_RATE, H_RATE, and O_RATE
 
         # columns that we don't want to change
-        columns_to_use = fho_df.columns[0:-3]
-        fho_copy = fho_df.copy(deep=True)
-        linetype_data = pd.melt(fho_copy, id_vars=list(columns_to_use), var_name='stat_name',
+        columns_to_use:pd.Index = fho_df.columns[0:-3]
+        fho_copy:pd.DataFrame = fho_df.copy(deep=True)
+        linetype_data:pd.DataFrame = pd.melt(fho_copy, id_vars=list(columns_to_use), var_name='stat_name',
                                 value_name='stat_value')
 
         # FHO line type doesn't have the bcl and bcu stat values set these to NA
-        na_column = ['NA' for na_column in range(0, linetype_data.shape[0])]
+        na_column:List[str] = ['NA' for na_column in range(0, linetype_data.shape[0])]
 
-        linetype_data['stat_ncl'] = na_column
-        linetype_data['stat_ncu'] = na_column
-        linetype_data['stat_bcl'] = na_column
-        linetype_data['stat_bcu'] = na_column
+        linetype_data['stat_ncl']:pd.Series = na_column
+        linetype_data['stat_ncu']:pd.Series = na_column
+        linetype_data['stat_bcl']:pd.Series = na_column
+        linetype_data['stat_bcu']:pd.Series = na_column
 
         return linetype_data
+
+    def process_cnt(self, stat_data:pd.DataFrame) -> pd.DataFrame:
+        '''
+           Reshape the data from the original MET output file (stat_data)
+        :param stat_data:
+        :return:
+        '''
 
 
 def main():
@@ -217,21 +228,20 @@ def main():
     '''
 
     # Acquire the output file name and output directory information and location of the xml specification file
-    config_file = util.read_config_from_command_line()
+    config_file:str = util.read_config_from_command_line()
     with open(config_file, 'r') as stream:
         try:
-            parms = yaml.load(stream, Loader=yaml.FullLoader)
+            parms:dict = yaml.load(stream, Loader=yaml.FullLoader)
         except yaml.YAMLError as exc:
             print(exc)
 
-
     # Read in the XML load file. This contains information about which MET output files are to be loaded.
-    xml_file = parms['xml_spec_file']
-    xml_loadfile_obj = XmlLoadFile(xml_file)
+    xml_file:str = parms['xml_spec_file']
+    xml_loadfile_obj:XmlLoadFile= XmlLoadFile(xml_file)
     xml_loadfile_obj.read_xml()
 
     # Read all of the data from the data files into a dataframe
-    rdf_obj = ReadDataFiles()
+    rdf_obj:ReadDataFiles = ReadDataFiles()
 
     # read in the data files, with options specified by XML flags
     rdf_obj.read_data(xml_loadfile_obj.flags,
@@ -239,9 +249,9 @@ def main():
                       xml_loadfile_obj.line_types)
 
     # Write stat file in ASCII format, one for each line type
-    stat_lines_obj = WriteStatAscii()
-    stat_lines_obj.write_stat_ascii(xml_loadfile_obj.flags,
-                                    rdf_obj.stat_data, parms)
+    stat_lines_obj:WriteStatAscii = WriteStatAscii()
+    stat_lines_obj.write_stat_ascii(rdf_obj.stat_data, parms)
+
 
 if __name__ == "__main__":
     main()
