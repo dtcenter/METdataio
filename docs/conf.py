591234832074acd181008d9cdea5f19b649c70ea
bikegeek <3753118+bikegeek@users.noreply.github.com>
2024-02-07 15:39:16 -0700

    Update conf.py
    change version to 'develop' to be consistent

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ print(sys.path)
 project = 'METdataio'
 author = 'UCAR/NCAR, NOAA, CSU/CIRA, and CU/CIRES'
 author_list = 'Fisher, H.,  M. Win-Gildenmeister, V. Hagerty, and T. Burek'
-version = '3.0.0'
+version = 'develop'
 verinfo = version
 release = f'{version}'
 release_year = '2023'
