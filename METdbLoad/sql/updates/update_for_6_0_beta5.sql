16504bea2a866a75f2c16f830e240c2b4cbe4640
bikegeek <minnawin@ucar.edu>
2024-06-24 14:30:45 -0600

    Issue #307 TOTAL_DIR for VL1L2, VAL1L2, and VCNT linetypes

---
+++ METdbLoad/sql/updates/update_for_6_0_beta5.sql
@@ -0,0 +1,13 @@
+DELIMITER |
+
+
+ALTER TABLE line_data_vcnt
+    ADD COLUMN total_dir DOUBLE |
+
+ALTER TABLE line_data_val1l2
+    ADD COLUMN total_dir DOUBLE |
+
+ALTER TABLE line_data_vl1l2
+    ADD COLUMN total_dir DOUBLE |
+
+DELIMITER ;
