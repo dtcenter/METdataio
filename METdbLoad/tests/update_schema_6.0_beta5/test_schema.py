f02a04a792d683b590e3e7f376601e660ca105c8
Minna Win <minnawin@mohawk.rap.ucar.edu>
2024-06-25 22:33:03 +0000

    update to separate the host and port and simplify test setup code

--- METdbLoad/tests/update_schema_6.0_beta5/test_schema.py
+++ METdbLoad/tests/update_schema_6.0_beta5/test_schema.py
@@ -30,47 +30,37 @@ def setup_db():
             print(exc)
 
     # Create a dataclass of the database information
-    # TCDiag = make_dataclass("TCDiag", ["total", "index", "diag_src", "diag_val"], frozen=True)
-    # orig = TCDiag(orig_total, orig_index, orig_diag_src, orig_diag_val)
-    DBS = make_dataclass("DBS", ["hostname", "username", "password", "dbname"])
-    db_settings = DBS(parms['hostname'], parms['username'], parms['password'], parms['dbname'])
+    DBS = make_dataclass("DBS", ["hostname", "port",  "username", "password", "dbname"])
+    db_settings = DBS(parms['hostname'], parms['port'], parms['username'], parms['password'], parms['dbname'])
 
     # Return the db connection object
-
-    ## settings (hostname, username, etc.)
-    ## yield db_settings
     conn = pymysql.connect(
         host=db_settings.hostname,
+        port=db_settings.port,
         user=db_settings.username,
         password=db_settings.password,
         db=db_settings.dbname,
         charset='utf8mb4'
     )
+    ## settings (hostname, username, etc.)
     yield conn
 
-    # Tear-down
-    conn.close()
 
 
 def test_tables_created(setup_db):
-
+    
     # log into the database and verify the VCNT, VL1L2, and VAL1L2 tables exist
 
-    try:
-        with setup_db.cursor() as cursor:
-            # Check that the  line_data_vcnt, line_data_vl1l2, and
-            # line_data_val1l2 tables were created
-            cursor.execute(CONST_LOAD_DB_CMD)
+    cursor = setup_db.cursor()
+    cursor.execute(CONST_LOAD_DB_CMD)
+    cursor.execute("show tables;")
 
-            check_tables_exist = "show tables;"
-            cursor.execute(check_tables_exist)
 
-            # Get all rows
-            rows = cursor.fetchall()
-            list_of_rows = [r[0] for r in rows]
-            assert 'line_data_vcnt' in list_of_rows
-            assert 'line_data_vl1l2' in list_of_rows
-            assert 'line_data_val1l2' in list_of_rows
+    # Get all rows
+    rows = cursor.fetchall()
+    list_of_rows = [r[0] for r in rows]
+    assert 'line_data_vcnt' in list_of_rows
+    assert 'line_data_vl1l2' in list_of_rows
+    assert 'line_data_val1l2' in list_of_rows
 
-    finally:
-        setup_db.close()
+    setup_db.close()
