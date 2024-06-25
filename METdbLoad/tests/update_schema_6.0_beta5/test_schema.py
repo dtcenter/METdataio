75e08b7f26d11acd3955b79cfe84314dd09be52c
bikegeek <minnawin@ucar.edu>
2024-06-24 19:05:50 -0600

    Issue #307 initial commit of test for VL1L2, VAL1L2, and VCNT for the TOTAL_DIR column

---
+++ METdbLoad/tests/update_schema_6.0_beta5/test_schema.py
@@ -0,0 +1,76 @@
+import pytest
+import pymysql
+import yaml
+from dataclasses import make_dataclass
+
+#######################################################################
+# These tests can only be run on the host where the database is running.
+# Pre-condition:
+#     The data in the accompanying data directory ./Data, should
+#     already be loaded in the database using the corresponding
+#     schema: mv_mysql.sql and the appropriate xml specification file.
+#     This is to avoid having the password visible in the test code.
+#
+
+CONST_LOAD_DB_CMD = "use mv_load_test"
+
+
+@pytest.fixture
+def setup_db():
+    """
+          Read in the config file to retrieve the database login information.
+
+    """
+    config_file = 'test_loading.yaml'
+    with open(config_file, 'r') as stream:
+        try:
+            parms: dict = yaml.load(stream, Loader=yaml.FullLoader)
+            # pathlib.Path(parms['output_dir']).mkdir(parents=True, exist_ok=True)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    # Create a dataclass of the database information
+    # TCDiag = make_dataclass("TCDiag", ["total", "index", "diag_src", "diag_val"], frozen=True)
+    # orig = TCDiag(orig_total, orig_index, orig_diag_src, orig_diag_val)
+    DBS = make_dataclass("DBS", ["hostname", "username", "password", "dbname"])
+    db_settings = DBS(parms['hostname'], parms['username'], parms['password'], parms['dbname'])
+
+    # Return the db connection object
+
+    ## settings (hostname, username, etc.)
+    ## yield db_settings
+    conn = pymysql.connect(
+        host=setup_db.hostname,
+        user=setup_db.username,
+        password=setup_db.password,
+        db=setup_db.dbname,
+        charset='utf8mb4'
+    )
+    yield conn
+
+    # Tear-down
+    conn.close()
+
+
+def test_tables_created(setup_db):
+
+    # log into the database and verify the VCNT, VL1L2, and VAL1L2 tables exist
+
+    try:
+        with setup_db.cursor() as cursor:
+            # Check that the  line_data_vcnt, line_data_vl1l2, and
+            # line_data_val1l2 tables were created
+            cursor.execute(CONST_LOAD_DB_CMD)
+
+            check_tables_exist = "show tables;"
+            cursor.execute(check_tables_exist)
+
+            # Get all rows
+            rows = cursor.fetchall()
+            list_of_rows = [r[0] for r in rows]
+            assert 'line_data_vcnt' in list_of_rows
+            assert 'line_data_vl1l2' in list_of_rows
+            assert 'line_data_val1l2' in list_of_rows
+
+    finally:
+        setup_db.close()
