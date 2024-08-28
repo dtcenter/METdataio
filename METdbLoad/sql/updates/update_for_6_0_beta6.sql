e4eb02016da48b756b04cac368bb5841b87210a2
bikegeek <3753118+bikegeek@users.noreply.github.com>
2024-08-28 14:48:45 -0600

    Update update_for_6_0_beta6.sql
    fixed the ADD COLUMN lines for ORANK

--- METdbLoad/sql/updates/update_for_6_0_beta6.sql
+++ METdbLoad/sql/updates/update_for_6_0_beta6.sql
@@ -11,8 +11,8 @@ ALTER TABLE line_data_mpr
 ALTER TABLE line_data_orank
     RENAME COLUMN climo_mean TO obs_climo_mean |
     RENAME COLUMN climo_stdev TO obs_climo_stdev |
-    ADD COLUMN total_dir DOUBLE |
-    ADD COLUMN total_dir DOUBLE |
+    ADD COLUMN fcst_climo_mean DOUBLE |
+    ADD COLUMN fcst_climo_stdev DOUBLE |
 |
 
 DELIMITER ;
