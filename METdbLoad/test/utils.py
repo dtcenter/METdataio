e419dc70eaaae02d3d118eb16622420e266d8ac2
bikegeek <minnawin@ucar.edu>
2024-09-25 17:41:17 -0600

    Change the name of the XML schema file

--- METdbLoad/test/utils.py
+++ METdbLoad/test/utils.py
@@ -8,7 +8,7 @@ def abs_path(rel_path):
     return str(Path(str(Path(__file__).parents[2])) / rel_path)
 
 # XML Schema for the load specification XML
-LOAD_SPECIFICATION_SCHEMA = os.path.join(abs_path("METdbLoad/ush/"), "load_specification.xsd")
+LOAD_SPECIFICATION_SCHEMA = os.path.join(abs_path("METdbLoad/ush/"), "load_specification_schema.xsd")
 
 # Use data from METreformat where available
 ENSEMBLE_STAT_DATA_DIR = abs_path("METreformat/test/data/ensemble_stat")
@@ -102,6 +102,7 @@ def get_xml_test_file(tmp_path, met_data_dir, met_tool, load_flags={}, local_inf
     xml_path = tmp_path / "test_load_specification.xml"
     with open(xml_path, "w") as text_file:
         text_file.write(populate_xml_load_spec(met_data_dir, met_tool, load_flags, local_infile))
+
     return xml_path
 
 
