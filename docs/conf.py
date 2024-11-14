c85b4f59edd913e547be47bcf3bcad6757b2c111
bikegeek <3753118+bikegeek@users.noreply.github.com>
2024-11-14 12:20:45 -0700

    Updating versions and adding release notes for RC1 (#353)

--- docs/conf.py
+++ docs/conf.py
@@ -18,13 +18,13 @@ print(sys.path)
 
 # -- Project information -----------------------------------------------------
 project = 'METdataio'
-author = 'UCAR/NCAR, NOAA, CSU/CIRA, and CU/CIRES'
+author = 'UCAR/NSF NCAR, NOAA, CSU/CIRA, and CU/CIRES'
 author_list = 'Fisher, H.,  M. Win-Gildenmeister, V. Hagerty, M. Smith, and T. Burek'
-version = '3.0.0-rc1-dev'
+version = '3.0.0-rc1'
 verinfo = version
 release = f'{version}'
 release_year = '2024'
-release_date = f'{release_year}-10-17'
+release_date = f'{release_year}-11-14'
 copyright = f'{release_year}, {author}'
 
 # if set, adds "Last updated on " followed by
