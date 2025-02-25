54828fa2c4d7bef396eabe49e27402ae496cf9aa
bikegeek <minnawin@ucar.edu>
2025-02-25 16:53:43 -0700

    set dtype of the FCST_LEAD and OBS_LEAD series earlier, to fix error when using pandas merge

--- METdbLoad/ush/read_data_files.py
+++ METdbLoad/ush/read_data_files.py
@@ -549,16 +549,16 @@ class ReadDataFiles:
                                         mtd_file[CN.FCST_LEAD_HR], unit='sec')
 
                                 # Where fcst_lead was set to zero for math, set it to -9999
+                                mtd_file[CN.FCST_LEAD].asdtype(int)
                                 if mtd_file[CN.FCST_LEAD].eq(0).any():
                                     mtd_file.loc[mtd_file.fcst_lead ==
-                                                 0, CN.FCST_LEAD] = int(CN.MV_NOTAV)
-                                    mtd_file[CN.FCST_LEAD].astype(object)
+                                                 0, CN.FCST_LEAD] = CN.MV_NOTAV
 
                                 # if OBS_LEAD is NA, set it to -9999
+                                mtd_file[CN.OBS_LEAD].astype(int)
                                 if not mtd_file.obs_lead.dtypes == 'int':
                                     mtd_file.loc[mtd_file.obs_lead ==
-                                                 CN.NOTAV, CN.OBS_LEAD] = int(CN.MV_NOTAV)
-                                    mtd_file[CN.OBS_LEAD].astype(object)
+                                                 CN.NOTAV, CN.OBS_LEAD] = CN.MV_NOTAV
 
                                 # initially, match line data to the index of the file names
                                 mtd_file[CN.FILE_ROW] = row_num
