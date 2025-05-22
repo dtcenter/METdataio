9f902db48ccd7b34c6c2c13a59009215ecd2f859
bikegeek <minnawin@ucar.edu>
2025-05-22 08:32:14 -0600

    Change date, needed to redo release

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ version = '3.1.0-rc1'
 verinfo = version
 release = f'{version}'
 release_year = '2025'
-release_date = f'{release_year}-05-21'
+release_date = f'{release_year}-05-22'
 copyright = f'{release_year}, {author}'
 
 # if set, adds "Last updated on " followed by
