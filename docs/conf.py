48f4b3c69b6efb85dfb01be91e2a7a8399bde8e6
MWin <3753118+bikegeek@users.noreply.github.com>
2026-05-07 13:55:20 -0600

    Update conf.py
    update version for beta3 (cycle3 ) dev cycle

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ print(sys.path)
 project = 'METdataio'
 author = 'UCAR/NSF NCAR, NOAA, CSU/CIRA, and CU/CIRES'
 author_list = 'Smith, M., T. Burek,  H. Fisher, M. Win-Gildenmeister, and V. Hagerty'
-version = '13.0.0-beta2'
+version = '13.0.0-beta3-dev'
 verinfo = version
 release = f'{version}'
 release_year = '2026'
