3b989e1ca800d6dab8cafdfd4a3a4162756ac6f5
bikegeek <minnawin@ucar.edu>
2026-07-09 16:45:43 -0600

    Add directory with one empty datafile for additional testing of try-except blocks in read_data_files.py

--- METdbLoad/test/utils.py
+++ METdbLoad/test/utils.py
@@ -17,6 +17,7 @@ MPR_DATA_DIR = abs_path("METreformat/test/data/mpr/climo_data")
 POINT_STAT_DATA_DIR = abs_path("METreformat/test/data/point_stat")
 TCDIAG_DATA_DIR = abs_path("METreformat/test/data/tcdiag_tcmpr")
 EMPTY_DIR = abs_path("METreformat/test/data/point_stat/empty")
+ONE_EMPTY_DIR = abs_path("METreformat/test/data/point_stat/one_empty")
 
 
 # This data is copied from MET test data
