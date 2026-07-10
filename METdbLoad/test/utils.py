992e069088a073156793a98667da9d9ddc45fb9c
bikegeek <minnawin@ucar.edu>
2026-07-10 13:13:48 -0600

    Additional data for testing MODE

--- METdbLoad/test/utils.py
+++ METdbLoad/test/utils.py
@@ -24,9 +24,11 @@ VSDB_DIR = abs_path("METdbLoad/test/data/vsdb")
 VSDB_NO_EQUALS_DIR = abs_path("METdbLoad/test/data/vsdb/vsdb_no_equals")
 VSDB_EMPTY = abs_path("METdbLoad/test/data/empty/empty_vsdb")
 VSDB_ONE_EMPTY = abs_path("METdbLoad/test/data/empty/empty_vsdb/one_empty")
-MODE_CTS_EMPTY = abs_path("METdbLoad/test/data/empty/mode_cts/")
-MODE_OBJ_EMPTY = abs_path("METdbLoad/test/data/empty/mode_obj/")
+MODE_CTS_EMPTY = abs_path("METdbLoad/test/data/empty/empty_mode_cts/")
+MODE_OBJ_EMPTY = abs_path("METdbLoad/test/data/empty/empty_mode_obj/")
 MODE_NO_HEADER = abs_path("METdbLoad/test/data/mode/no_header")
+MODE_ONLY_CTS = abs_path("METdbLoad/test/data/mode/only_cts")
+MODE_EMPTY = abs_path("METdbLoad/test/data/empty/empty_mode")
 
 
 # This data is copied from MET test data
